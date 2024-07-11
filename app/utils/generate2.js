5ed2f855273a51fb6840f93262e44bdb56153a0c
Daniel <danfanhanyuan@gmail.com>
2024-07-11 12:33:10 -0400

    changed puzzle generation for "winning position" and added new puzzles

--- app/utils/generate2.js
+++ app/utils/generate2.js
@@ -1,14 +1,19 @@
 
-
 const Generate = async () => {
-    const list = ["r4rk1/p1pp1ppp/bpP5/4Pp2/8/5R1P/PBP3N1/3R2K1 b - - 0 21"]
-    const correctMove = [["Be2"]]
+    const list = ["r4rk1/p1pp1ppp/bpP5/4Pp2/8/5R1P/PBP3N1/3R2K1 b - - 0 21","r2qk2r/2p2ppp/ppp5/2b1P3/5Pn1/2P5/PPQ3PP/RNB2R1K b kq - 4 14","r4rk1/2p2ppp/ppQ5/4q3/5B2/2P3n1/PP4PP/RN3RK1 b - - 2 16"]
     try {
-      const board = list[0]
+      const response = await fetch("/positions.txt");
+      const text = await response.text();
+      const lines = text.split('\n');
+      const random = Math.floor(Math.random() * (lines.length-1 + 1));
+      const board = lines[random]
       var index = 0
       var rowpos = 7
-      const moves = correctMove[0]
-      
+      const response2 = await fetch("/moves.txt");
+      const text2 = await response2.text();
+      const lines2 = text2.split('\n');
+      console.log(lines2)
+      const moves = [lines2[random]]
       var whitepawn = []
       var whiteknight = []
       var whiteQueen = []
