4a919ef17c0fc66e52f7e175dab12fde8f9004b2
Daniel <danfanhanyuan@gmail.com>
2025-08-28 17:36:29 -0400

    Chess-Vision-Training

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDc7yDZiL9_dReDTqCNsTZdNZCjyK7oNOM",
+  authDomain: "chessanalytic.firebaseapp.com",
+  projectId: "chessanalytic",
+  storageBucket: "chessanalytic.appspot.com",
+  messagingSenderId: "839639951843",
+  appId: "1:839639951843:web:07cb7082c2a6ccd8b318d6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
